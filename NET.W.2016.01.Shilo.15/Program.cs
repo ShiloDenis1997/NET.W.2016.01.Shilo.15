b6ced239dc493e5b7f4860d603c03161cdcfd2e6
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-25 16:55:08 +0300

    Добавьте файлы проекта.

---
+++ NET.W.2016.01.Shilo.15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET.W._2016._01.Shilo._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
