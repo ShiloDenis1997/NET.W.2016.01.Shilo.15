aeab70b02032581e1111dbbc9d8ace5bb02cca8a
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-27 15:52:36 +0300

    First test for matrix extension added. It's green

---
+++ Task1.ExtensionLogicTests/MatrixExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Task1.ExtensionLogic;
+using Task1.Logic;
+
+namespace Task1.ExtensionLogicTests
+{
+    [TestFixture]
+    public class MatrixExtensionsTests
+    {
+        [Test]
+        public void MatrixSum_IntSquareSquare_SumMatrixExpected()
+        {
+            int[,] matrix = 
+            {
+                {1, 2, 3, 4},
+                {1, 2, 3, 4},
+                {5, 2, 3, 6},
+                {3, 5, 2, 1},
+            };
+            AbstractSquareMatrix<int> firstMatrix = new SquareMatrix<int>(matrix);
+            AbstractSquareMatrix<int> secondMatrix = new SquareMatrix<int>(matrix);
+            var resultMatrix = (SquareMatrix<int>)MatrixExtensions.MatrixSum(firstMatrix, secondMatrix,
+                (a, b) => a + b);
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    Assert.AreEqual(matrix[i, j]*2, resultMatrix[i, j]);
+        }
+    }
+}
