9a2b927b9ca616e6253fb1c753b2cf16550a4d90
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-27 17:39:45 +0300

    Matrix extension tests added

--- Task1.ExtensionLogicTests/MatrixExtensionsTests.cs
+++ Task1.ExtensionLogicTests/MatrixExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Task1.ExtensionLogic;
 using Task1.Logic;
 
@@ -19,8 +20,8 @@ namespace Task1.ExtensionLogicTests
             };
             AbstractSquareMatrix<int> firstMatrix = new SquareMatrix<int>(matrix);
             AbstractSquareMatrix<int> secondMatrix = new SquareMatrix<int>(matrix);
-            var resultMatrix = (SquareMatrix<int>)MatrixExtensions.MatrixSum(firstMatrix, secondMatrix,
-                (a, b) => a + b);
+            var resultMatrix = (SquareMatrix<int>)MatrixExtensions.MatrixSum
+                (firstMatrix, secondMatrix, (a, b) => a + b);
             for (int i = 0; i < resultMatrix.Dimension; i++)
                 for (int j = 0; j < resultMatrix.Dimension; j++)
                     Assert.AreEqual(matrix[i, j]*2, resultMatrix[i, j]);
@@ -54,6 +55,103 @@ namespace Task1.ExtensionLogicTests
             for (int i = 0; i < resultMatrix.Dimension; i++)
                 for (int j = 0; j < resultMatrix.Dimension; j++)
                     Assert.AreEqual(matrix[i, j].X * 2, resultMatrix[i, j].X);
+        }
+
+        [Test]
+        public void MatrixSum_IntSquareSymmetric_SumMatrixExpected()
+        {
+            int[,] matrix =
+            {
+                {1,    2, 3, 4},
+                {1, 2,    3, 4},
+                {5, 2, 3,    6},
+                {3, 5, 2, 1},
+            };
+            AbstractSquareMatrix<int> firstMatrix = new SquareMatrix<int>(matrix);
+            AbstractSquareMatrix<int> secondMatrix = new SymmetricMatrix<int>(matrix);
+            var resultMatrix = (SquareMatrix<int>)MatrixExtensions.MatrixSum
+                (firstMatrix, secondMatrix, (a, b) => a + b);
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    Assert.AreEqual(matrix[i, j] + matrix[Math.Max(i, j), Math.Min(i, j)],
+                        resultMatrix[i, j]);
+        }
+
+        [Test]
+        public void MatrixSum_IntWrapperSquareSymmetric_SumMatrixExpected()
+        {
+            IntWrapper[,] matrix =
+            {
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(5), new IntWrapper(2), new IntWrapper(3), new IntWrapper(6)},
+                {new IntWrapper(3), new IntWrapper(5), new IntWrapper(2), new IntWrapper(1)},
+            };
+            AbstractSquareMatrix<IntWrapper> firstMatrix =
+                new SquareMatrix<IntWrapper>(matrix);
+            AbstractSquareMatrix<IntWrapper> secondMatrix =
+                new SymmetricMatrix<IntWrapper>(matrix);
+            var resultMatrix = (SquareMatrix<IntWrapper>)MatrixExtensions
+                .MatrixSum(firstMatrix, secondMatrix, (a, b) => new IntWrapper(a.X + b.X));
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    Assert.AreEqual(matrix[i, j].X + matrix[Math.Max(i, j), Math.Min(i, j)].X,
+                        resultMatrix[i, j].X);
+        }
+
+        [Test]
+        public void MatrixSum_IntWrapperSymmetricDiagonal_SumMatrixExpected()
+        {
+            IntWrapper[,] matrix =
+            {
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(5), new IntWrapper(2), new IntWrapper(3), new IntWrapper(6)},
+                {new IntWrapper(3), new IntWrapper(5), new IntWrapper(2), new IntWrapper(1)},
+            };
+            IntWrapper[] diagonal = 
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)};
+            AbstractSquareMatrix<IntWrapper> firstMatrix =
+                new SymmetricMatrix<IntWrapper>(matrix);
+            AbstractSquareMatrix<IntWrapper> secondMatrix =
+                new DiagonalMatrix<IntWrapper>(diagonal);
+            var resultMatrix = (SquareMatrix<IntWrapper>)MatrixExtensions
+                .MatrixSum(firstMatrix, secondMatrix, (a, b) => 
+                new IntWrapper((a?.X ?? 0) + (b?.X ?? 0)));
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    if (i == j)
+                        Assert.AreEqual(matrix[i, j].X + diagonal[i].X,
+                            resultMatrix[i, j].X);
+                    else
+                    {
+                        Assert.AreEqual(matrix[Math.Max(i, j), Math.Min(i, j)].X, 
+                            resultMatrix[i, j].X);
+                    }
+        }
+
+        [Test]
+        public void MatrixSum_IntSymmetricDiagonal_SumMatrixExpected()
+        {
+            int[,] matrix =
+            {
+                {1,    2, 3, 4},
+                {1, 2,    3, 4},
+                {5, 2, 3,    6},
+                {3, 5, 2, 1},
+            };
+            int[] diagonal = {1, 2, 3, 4};
+            AbstractSquareMatrix<int> firstMatrix = new SymmetricMatrix<int>(matrix);
+            AbstractSquareMatrix<int> secondMatrix = new DiagonalMatrix<int>(diagonal);
+            var resultMatrix = (SquareMatrix<int>)MatrixExtensions.MatrixSum
+                (firstMatrix, secondMatrix, (a, b) => a + b);
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    if (i == j)
+                        Assert.AreEqual(matrix[i, j] + diagonal[i], resultMatrix[i, j]);
+                    else
+                        Assert.AreEqual(matrix[Math.Max(i, j), Math.Min(i, j)],
+                            resultMatrix[i, j]);
         }
     }
 }
