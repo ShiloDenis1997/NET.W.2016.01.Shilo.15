9d7112022536ebcc9e5ea99ff235c919959802e0
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-27 17:20:05 +0300

    SquareMatrix ref type test added and passed.

--- Task1.ExtensionLogicTests/MatrixExtensionsTests.cs
+++ Task1.ExtensionLogicTests/MatrixExtensionsTests.cs
@@ -25,5 +25,35 @@ namespace Task1.ExtensionLogicTests
                 for (int j = 0; j < resultMatrix.Dimension; j++)
                     Assert.AreEqual(matrix[i, j]*2, resultMatrix[i, j]);
         }
+
+        public class IntWrapper
+        {
+            public int X { get; set; }
+
+            public IntWrapper(int x)
+            {
+                X = x;
+            }
+        }
+        [Test]
+        public void MatrixSum_IntWrapperSquareSquare_SumMatrixExpected()
+        {
+            IntWrapper[,] matrix =
+            {
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(1), new IntWrapper(2), new IntWrapper(3), new IntWrapper(4)},
+                {new IntWrapper(5), new IntWrapper(2), new IntWrapper(3), new IntWrapper(6)},
+                {new IntWrapper(3), new IntWrapper(5), new IntWrapper(2), new IntWrapper(1)},
+            };
+            AbstractSquareMatrix<IntWrapper> firstMatrix = 
+                new SquareMatrix<IntWrapper>(matrix);
+            AbstractSquareMatrix<IntWrapper> secondMatrix = 
+                new SquareMatrix<IntWrapper>(matrix);
+            var resultMatrix = (SquareMatrix<IntWrapper>) MatrixExtensions
+                .MatrixSum(firstMatrix, secondMatrix, (a, b) => new IntWrapper(a.X + b.X));
+            for (int i = 0; i < resultMatrix.Dimension; i++)
+                for (int j = 0; j < resultMatrix.Dimension; j++)
+                    Assert.AreEqual(matrix[i, j].X * 2, resultMatrix[i, j].X);
+        }
     }
 }
