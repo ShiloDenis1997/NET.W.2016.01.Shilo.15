1f0e23aeeb3be28a79e42f88a005ce961bc66208
Shilo Denis <shilo.denis.1997g@gmail.com>
2016-11-27 14:07:08 +0300

    Non-extension realization of squareMatrix done

---
+++ Task1.Logic/AbstractSquareMatrix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task1.Logic
+{
+    public abstract class AbstractSquareMatrix<T>
+    {
+        private int dimension;
+        public event EventHandler<ElementChangedEventArgs> elementChangedEvent; 
+
+        public int Dimension {
+            get { return dimension; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(value)} is less or equal to zero");
+                dimension = value;
+            }
+        }
+
+    }
+}
---
+++ Task1.Logic/SquareMatrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    public class SquareMatrix<T> : AbstractSquareMatrix<T>
+    {
+        private T[,] matrix;
+
+        public SquareMatrix(int dimension)
+        {
+            Dimension = dimension;
+            matrix = new T[dimension, dimension];
+        }
+
+        public SquareMatrix(T[,] matrix)
+        {
+            Dimension = matrix.GetLength(0);
+            this.matrix = new T[Dimension, Dimension];
+            for (int i = 0; i < Dimension; i++)
+                for (int j = 0; j < Dimension; j++)
+                    this.matrix[i, j] = matrix[i, j];
+        }
+
+        public T this[int row, int column]
+        {
+            get
+            {
+                if (row < 0 || row >= Dimension)
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(row)} is not in range with current dimension");
+                if (column < 0 || column >= Dimension)
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(column)} is not in range with current dimension");
+                return matrix[row, column];
+            }
+            set
+            {
+                if (row < 0 || row >= Dimension)
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(row)} is not in range with current dimension");
+                if (column < 0 || column >= Dimension)
+                    throw new ArgumentOutOfRangeException
+                        ($"{nameof(column)} is not in range with current dimension");
+                matrix[row, column] = value;
+            }
+        }
+    }
+}
